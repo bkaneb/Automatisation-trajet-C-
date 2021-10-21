e94d39d1d3a6d5fe2174035cb7b6772671cf79c2
bkaneb <bryankaneb2@gmail.com>
2021-10-21 18:15:06 +0200

    add page position

---
+++ page-position/Position.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Position
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.circuit1;
+            this.panel1.Location = new System.Drawing.Point(27, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(764, 285);
+            this.panel1.TabIndex = 7;
+            // 
+            // Position
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(830, 345);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Position";
+            this.Text = "Position";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fermeture_FormClosing);
+            this.Load += new System.EventHandler(this.Position_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
